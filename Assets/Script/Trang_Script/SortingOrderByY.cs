16aa0259e85f8dc9ca50878c17bcb3cc29233372
TTYSH <trang123123100@gmail.com>
2025-11-21 16:30:42 +0700

    fix sorting order
    fix sorting order

--- Assets/Script/Trang_Script/SortingOrderByY.cs
+++ Assets/Script/Trang_Script/SortingOrderByY.cs
@@ -1,20 +1,18 @@
 using UnityEngine;
+using UnityEngine.Rendering; // b?t bu?c ?? dùng SortingGroup
 
-[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(SortingGroup))]
 public class SortingOrderByY : MonoBehaviour
 {
-    private SpriteRenderer spriteRenderer;
+    private SortingGroup sortingGroup;
 
     void Awake()
     {
-        // L?y component SpriteRenderer g?n v?i object
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        sortingGroup = GetComponent<SortingGroup>();
     }
 
     void LateUpdate()
     {
-        // C?p nh?t th? t? v? d?a trên v? trí Y
-        // Giá tr? càng nh? (?i xu?ng d??i), layer càng cao ? hi?n th? phía tr??c
-        spriteRenderer.sortingOrder = -(int)(transform.position.y * 100);
+        sortingGroup.sortingOrder = -(int)(transform.position.y * 100);
     }
 }
